3bd5c9a78ceb70b8e66a0aba92a9702c942521a4
FLoyd Dameron <fdameron@opticat.net>
2024-05-03 12:57:29 -0600

    initial commit

--- DetectChanges/Program.cs
+++ DetectChanges/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OCEntity.OnBoard;
 
 namespace DetectChanges
 {
@@ -10,6 +13,43 @@ namespace DetectChanges
     {
         static void Main(string[] args)
         {
+            var log = File.AppendText("c:\\logs\\DetectChanges.txt");
+            var s = new Stopwatch();
+            s.Start();
+            using(var ctxOB = new OnboardEntities())
+            {
+                var myfd = ctxOB.imagesync_filedatas.ToList();
+                foreach(var item in myfd)
+                {
+                    item.LastISSRenamingEnabled = false;
+                    //ctxOB.SaveChanges();
+                }
+                ctxOB.SaveChanges();
+            }
+            s.Stop();
+            Console.WriteLine("AutoDetectChangesEnabled = true  Total Seconds:" + s.Elapsed.TotalSeconds);
+            log.WriteLine("AutoDetectChangesEnabled = true  Total Seconds:" + s.Elapsed.TotalSeconds);
+            log.Flush();
+            s.Reset();
+            s.Start();
+            using(var ctxOB = new OnboardEntities())
+            {
+                ctxOB.Configuration.AutoDetectChangesEnabled = false;
+                var myfd = ctxOB.imagesync_filedatas.ToList();
+                foreach(var item in myfd)
+                {
+                    item.LastISSRenamingEnabled = true;
+                    //ctxOB.ChangeTracker.DetectChanges();
+                    //ctxOB.SaveChanges();
+                }
+                ctxOB.ChangeTracker.DetectChanges();
+                ctxOB.SaveChanges();
+            }
+            s.Stop();
+            Console.WriteLine("AutoDetectChangesEnabled = false  Total Seconds:" + s.Elapsed.TotalSeconds);
+            log.WriteLine("AutoDetectChangesEnabled = false  Total Seconds:" + s.Elapsed.TotalSeconds);
+            log.Flush();
+            log.Close();
         }
     }
 }
